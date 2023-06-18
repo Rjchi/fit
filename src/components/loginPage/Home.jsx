d9db94587a8b3adf17bc019be001337b6482bd20
Rjchi <richardospina18@gmail.com>
2023-06-18 12:40:35 -0500

    Estilizar pagina de login

--- src/components/loginPage/Home.jsx
+++ src/components/loginPage/Home.jsx
@@ -3,13 +3,22 @@ import { useState } from "react";
 import { login } from "../../redux/actions/login/login";
 import { Link, Navigate } from "react-router-dom";
 // import MyImg from "../../assets/imagen.png";
+import Visi from "../../assets/visi.png";
+import noVisi from "../../assets/noVisi.png";
+import Logo from "../../assets/logo.png";
 import styles from "../../styles/Home.module.css";
 
 const Home = ({ login, isAuthenticated, loading, user }) => {
+  const [see, setSee] = useState(false);
   const [formData, setFormData] = useState({
     password: "",
     email: "",
   });
+
+  const onclick = (e) => {
+    e.preventDefault();
+    setSee(!see);
+  };
 
   const { password, email } = formData;
 
@@ -34,10 +43,7 @@ const Home = ({ login, isAuthenticated, loading, user }) => {
     <div className={styles.ContainerG}>
       <div className={styles.ContainerP}>
         <div className={styles.Logo}>
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFzCVcXgPapeM3P1NGOGJDaKCzqxwYgIyzgw&usqp=CAU"
-            alt="logo"
-          />
+          <img src={Logo} alt="logo" />
           <p>Fitness</p>
         </div>
         <h1>LOGIN</h1>
@@ -50,36 +56,51 @@ const Home = ({ login, isAuthenticated, loading, user }) => {
             action="#"
             method="POST"
           >
-              <div className={styles.Inputs}>
-                <label htmlFor="email-address">EMAIL ADDRESS</label>
-                <input
-                  id="email-address"
-                  name="email"
-                  value={email}
-                  onChange={(e) => onChange(e)}
-                  type="email"
-                  autoFocus
-                  required
-                  placeholder="Email address"
-                />
-              </div>
-              <div className={styles.Inputs}>
-                <label htmlFor="password">PASSWORD</label>
+            <div className={styles.Inputs}>
+              <label htmlFor="email-address">EMAIL ADDRESS</label>
+              <input
+                id="email-address"
+                name="email"
+                value={email}
+                onChange={(e) => onChange(e)}
+                type="email"
+                autoFocus
+                required
+                placeholder="Email address"
+              />
+            </div>
+            <div className={styles.Inputs}>
+              <label htmlFor="password">PASSWORD</label>
+              <div className={styles.Eye}>
                 <input
                   id="password"
                   name="password"
                   value={password}
                   onChange={(e) => onChange(e)}
-                  type="password"
+                  type={!see ? "password" : "text"}
                   required
                   placeholder="Password"
                 />
+                <p
+                  onClick={(e) => {
+                    onclick(e);
+                  }}
+                >
+                  {!see ? (
+                    <img src={noVisi} alt="eye" />
+                  ) : (
+                    <>
+                      <img src={Visi} alt="eye" />
+                    </>
+                  )}
+                </p>
               </div>
+            </div>
 
             <div className={styles.Links}>
-                <Link to="/new_account">Sign Up</Link>
+              <Link to="/new_account">Sign Up</Link>
 
-                <Link to="#">Forgot your password?</Link>
+              <Link to="#">Forgot your password?</Link>
             </div>
 
             <div>
